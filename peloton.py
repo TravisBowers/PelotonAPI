a8f51eff251a1ffc2393fb84fdd8bfb04f1c173e
Travis Bowers <travis.bowers4.ctr@army.mil>
2023-03-01 20:43:05 -0500

    modified workout metrics function to extract useful information from responses

--- peloton.py
+++ peloton.py
@@ -22,15 +22,51 @@ def get_completed_rides(session, user_id):
     url = "https://api.onepeloton.com/api/user/{id}/workouts?status=COMPLETE?fitness_discipline=cycling".format(id = user_id)
     data = session.get(url).json()
     df_workouts_raw = json_normalize(data['data'])
+    #print(json.dumps(data, indent=4))
     return df_workouts_raw
 
 def get_workout_metrics(session, workout_id):
-    url = 'https://api.onepeloton.com/api/workout/{}/performance_graph'.format(workout_id)
+    url = 'https://api.onepeloton.com/api/workout/{}'.format(workout_id)
     response = session.get(url)
     data = response.json()
-    print(type(workout_id))
-    print("getting workout metrics for workout:" + workout_id)
-    print(data)
+    #print(json.dumps(data, indent=4))
+
+    #extract useful values
+    difficulty = data['ride']['difficulty_rating_avg']
+    title = data['ride']['title']
+    description = data['ride']['description']
+    duration = data['ride']['duration']
+    image_url = data['ride']['image_url']
+    instructor_id = data['ride']['instructor_id']
+    class_id = data['ride']['id']
+    class_url = "https://members.onepeloton.com/classes/cycling?modal=classDetailsModal&classId={}".format(class_id)
+    ftp_info = data['ftp_info']
+    leaderboard_rank = data['leaderboard_rank']
+    total_leaderboard_users = data['total_leaderboard_users']
+    leaderboard_percentile = 1.0 - (float(leaderboard_rank)/float(total_leaderboard_users))
+
+    # get performance data
+    url = 'https://api.onepeloton.com/api/workout/{}/performance_graph?every_n=500'.format(workout_id)
+    response = session.get(url)
+    data = response.json()
+    metrics = data['metrics']
+    print(json.dumps(metrics, indent=4))
+    #output_values = data['metrics'][0]['values']
+    #cadence_values = data['metrics'][1]['values']
+    #resistance_value = data['metrics'][2]['values']
+    output_values = list(filter(lambda x:x['slug'] == "output", metrics))[0]['values']
+    cadence_values = list(filter(lambda x:x['slug'] == "cadence", metrics))[0]['values']
+    resistance_values = list(filter(lambda x:x['slug'] == "resistance", metrics))[0]['values']
+    speed_values = list(filter(lambda x:x['slug'] == "speed", metrics))[0]['values']
+    heart_rate_values = list(filter(lambda x:x['slug'] == "heart_rate", metrics))[0]['values']
+
+
+    print(leaderboard_percentile)
+
+    #print(json.dumps(output_values, indent=4))
+
+
+    quit()
     df_workout_metrics = json_normalize(data['metrics'])
     return df_workout_metrics
 
@@ -49,7 +85,7 @@ def main():
     for i in workout_ids:
         workout_id = i[0]
         metrics = get_workout_metrics(s, workout_id)
-        print(metrics.to_string())
+        #print(metrics.to_string())
 
     #print(workout_ids)
 
