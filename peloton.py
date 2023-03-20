373c3fb381b58465a008b231486ea40457146b09
Travis Bowers <tcb@epiphyte.org>
2023-03-19 20:55:32 -0400

    updated requirements to include jmespath and jinja2, started workout page template

--- peloton.py
+++ peloton.py
@@ -4,7 +4,7 @@ from pandas import json_normalize
 import os
 import datetime
 from matplotlib import pyplot as plt
-
+import jmespath
 import numpy as np
 
 MAX_WORKOUTS = 2
@@ -34,12 +34,37 @@ def get_workout_data(session, workout_id):
     url = 'https://api.onepeloton.com/api/workout/{}'.format(workout_id)
     response = session.get(url)
     data = response.json()
-    #print(json.dumps(data, indent=4))
+    url = 'https://api.onepeloton.com/api/workout/{id}/performance_graph?every_n={metrics_resolution}'.format(id=workout_id, metrics_resolution=METRICS_RESOLUTION)
+    response = session.get(url)
+    performance_graph = response.json()
+    data.update(performance_graph)
 
-    #extract useful values
-    workout_dict={}
+    return data
+
+def get_workout_data_list(s, workout_id_list):
+    workout_data_list = []
+    print("Gathering data for all completed rides")
+    for i in workout_id_list:
+        workout_id = i[0]
+        print("getting data for workout id: "+ str(workout_id))
+        workout_data_list.append(get_workout_data(s, workout_id))
+    return workout_data_list
+
+def generate_workout_page( workout_dict )
     
-    
+    # print(json.dumps(data, indent=4))
+    # print(str(type(data)))
+    metrics = performance_graph['metrics']
+    output_values = list(filter(lambda x:x['slug'] == "output", metrics))[0]['values']
+    cadence_values = list(filter(lambda x:x['slug'] == "cadence", metrics))[0]['values']
+    resistance_values = list(filter(lambda x:x['slug'] == "resistance", metrics))[0]['values']
+    speed_values = list(filter(lambda x:x['slug'] == "speed", metrics))[0]['values']
+    heart_rate_values = list(filter(lambda x:x['slug'] == "heart_rate", metrics))[0]['values']
+    heart_rate_zones = list(filter(lambda x:x['slug'] == "heart_rate", metrics))[0]['zones']
+    # plt.plot(np.array(heart_rate_values), '.')
+    # plt.show()
+    # print(workout_date)
+    # input(' press enter to continue')
     created_at = data['created_at']
     workout_dict['workout_date'] = datetime.datetime.fromtimestamp(created_at)
     workout_dict['difficulty'] = data['ride']['difficulty_rating_avg']
@@ -55,38 +80,6 @@ def get_workout_data(session, workout_id):
     total_leaderboard_users = data['total_leaderboard_users']
     #leaderboard_percentile = 1.0 - (float(leaderboard_rank)/float(total_leaderboard_users))
 
-    # get performance data
-    url = 'https://api.onepeloton.com/api/workout/{id}/performance_graph?every_n={metrics_resolution}'.format(id=workout_id, metrics_resolution=METRICS_RESOLUTION)
-    response = session.get(url)
-    performance_graph = response.json()
-    data.update(performance_graph)
-
-     
-    
-    # print(json.dumps(data, indent=4))
-    # print(str(type(data)))
-    metrics = performance_graph['metrics']
-    output_values = list(filter(lambda x:x['slug'] == "output", metrics))[0]['values']
-    cadence_values = list(filter(lambda x:x['slug'] == "cadence", metrics))[0]['values']
-    resistance_values = list(filter(lambda x:x['slug'] == "resistance", metrics))[0]['values']
-    speed_values = list(filter(lambda x:x['slug'] == "speed", metrics))[0]['values']
-    heart_rate_values = list(filter(lambda x:x['slug'] == "heart_rate", metrics))[0]['values']
-    heart_rate_zones = list(filter(lambda x:x['slug'] == "heart_rate", metrics))[0]['zones']
-    # plt.plot(np.array(heart_rate_values), '.')
-    # plt.show()
-    # print(workout_date)
-    # input(' press enter to continue')
-    return data
-
-def get_workout_data_list(s, workout_id_list):
-    workout_data_list = []
-    print("Gathering data for all completed rides")
-    for i in workout_id_list:
-        workout_id = i[0]
-        print("getting data for workout id: "+ str(workout_id))
-        workout_data_list.append(get_workout_data(s, workout_id))
-    return workout_data_list
-
 
 def main():
     user = os.getenv('PELOTON_USER')
@@ -99,12 +92,16 @@ def main():
     df_workout_ids = workouts.filter(['id'], axis=1)
     workout_ids = df_workout_ids.values.tolist()
     print("Gathering data for "+ str(len(workout_ids))+ " workouts")
-    quit()
+    #quit()
     workout_dataset = get_workout_data_list(s,workout_ids)
     print(str(type(workout_dataset)))
     print("data for each workout is stored as type: "+ str(type(workout_dataset[0])))
-
-
+    print(json.dumps(workout_dataset, indent=4))
+    
+    dates = jmespath.search('[*].created_at',workout_dataset)
+    ftps = jmespath.search('[*].ftp_info.ftp',workout_dataset)
+    
+  
 
 
 
